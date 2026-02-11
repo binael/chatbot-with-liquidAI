1bb8569b7e7367a588d65a5b286839e848ef71e0
binael <nbinael@yahoo.com>
2026-02-11 11:31:09 +0100

    add: added a LoRa fine-tuned model

---
+++ chat_bot_app/get_model_response.py
@@ -0,0 +1,32 @@
+# import torch
+# from transformers import AutoTokenizer, AutoModelForCausalLM
+# from peft import PeftModel
+
+
+# base = "LiquidAI/LFM2.5-1.2B-Instruct"
+# adapter = "./prime_robotics_lora"
+
+# tokenizer = AutoTokenizer.from_pretrained(adapter)
+
+# model = AutoModelForCausalLM.from_pretrained(
+#     base,
+#     torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32
+#     # Removed device_map="auto"
+# )
+
+# model = PeftModel.from_pretrained(model, adapter)
+
+# # Explicitly move model to GPU if available after loading the adapter
+# if torch.cuda.is_available():
+#     model = model.to("cuda")
+
+# print(" Model loaded successfully\n")
+
+# model.eval()
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("LiquidAI/LFM2.5-1.2B-Instruct")
+model = AutoModelForCausalLM.from_pretrained("LiquidAI/LFM2.5-1.2B-Instruct")
+
+print("Model loaded successfully\n")
